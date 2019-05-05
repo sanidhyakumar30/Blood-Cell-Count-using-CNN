372eb2bcc5bf13d24db335d4ee370ec36f3f84d3
sanidhyakumar30 <kumarsanidhya@gmail.com>
2019-05-05 20:36:19 +0530

    blood cell count

---
+++ GITBlood/images/head.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib import patches
+# read the csv file using read_csv function of pandas
+train = pd.read_csv(‘train_labels.csv’)
+train.head()
